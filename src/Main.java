a855baa8cb578c28dcb0434ae768234efec6b561
kokres <kokres@wp.pl>
2024-10-17 13:15:57 +0200

    drugi

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("git 1");
+        System.out.print("git 2");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
