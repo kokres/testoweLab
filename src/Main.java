5f6257b90aba027d51985d90974d9e69a845888b
kokres <kokres@wp.pl>
2024-10-17 13:17:28 +0200

    tzrzeci

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("git 2");
+        System.out.print("git 3");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
